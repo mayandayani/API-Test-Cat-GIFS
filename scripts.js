bdf9e26c564887133e6806e7c547c2a1ffc5e352
Mayan Dayani <85851994+mayandayani@users.noreply.github.com>
2022-01-07 13:27:04 -0800

    Modifications with API
    Updated code using fetch and extract

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=mezLDCmuL81DufLkufDkHWFl69TmMb0B&s=cats', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-});
+
+async function getCats() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=mezLDCmuL81DufLkufDkHWFl69TmMb0B&s=cats', {mode: 'cors'})
+    const catData = await response.json();
+    img.src = catData.data.images.original.url;
+}
+
+getCats();
 
 let refreshbtn = document.getElementById("btn");
 
